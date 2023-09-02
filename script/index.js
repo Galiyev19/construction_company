b980e2502c9cac8218bfa7afd8909ad083741d3d
Galiyev19 <galiyevalisher7@gmail.com>
2023-09-02 19:49:00 +0600

    first push

---
+++ script/index.js
@@ -0,0 +1,18 @@
+var menu = [
+  "Ramada plaza & Dar Tower",
+  "Hampton by Hilton",
+  "Sunrise",
+  "Era Palace",
+];
+
+const swiper = new Swiper(".swiper", {
+  direction: "horizontal",
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination", // Use your custom pagination container
+    clickable: true, // Make pagination buttons clickable
+    renderBullet: function (index, className) {
+      return '<span class="' + className + '">' + menu[index] + "</span>";
+    },
+  },
+});
